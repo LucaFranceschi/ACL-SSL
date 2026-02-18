7ce1d92f68603cca4ca3858b3250532e31e2c122
LucaFranceschi <luca.franceschi01@estudiant.upf.edu>
2026-02-18 20:35:26 +0100

    Small changes

---
+++ scripts/16-get_val_audios.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+set -e
+
+mkdir -p datasets/vggsound/val_audios
+
+cp datasets/vggsound/audio/---g-f_I2yQ_000001.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/--i-y1v8Hy8_000000.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-0jeONf82dE_000021.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-0pJqpNjft4_000267.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-0UuUoXQUoI_000107.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-0yRK50zyTI_000030.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-1EXhfqLLwQ_000150.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-2-wdcN5vOw_000017.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-2Dm0VjW8oM_000001.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-2sE5CH8Wb8_000030.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-2xiZDEuHd8_000030.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-3Kv4fdm7Uk_000030.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-3rHVsIj1M8_000030.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-3YWuPXHknk_000030.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-4DpBHTuc88_000075.wav datasets/vggsound/val_audios
+cp datasets/vggsound/audio/-40nDU5Ecgg_000130.wav datasets/vggsound/val_audios
