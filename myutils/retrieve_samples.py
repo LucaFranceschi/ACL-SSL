13b1918a9a1b99889766022c4db0baf903d023a9
LucaFranceschi <luca.franceschi01@estudiant.upf.edu>
2026-05-19 16:47:08 +0200

    Checkpoint 4

--- myutils/retrieve_samples.py
+++ myutils/retrieve_samples.py
@@ -1,8 +1,18 @@
 """
 Apart of serving the purpose I need this file for, I will also use it as an example on the use of AI in this project.
 
-Prompt used: generate the template / boilerplate code for a python script that has to parse a path argument, a best/worst argument, and a positive integer n
+Prompts used:
+    1. generate the template / boilerplate code for a python script that has to parse a path argument, a best/worst argument, and a positive integer n
+    2. python retrieve_samples.py --mode best -n 10 ../train_outputs/2214807/Test_record/ACL_ViT16_Exp_ACL_v1/dumps/epochbest
+        Path: ../train_outputs/2214807/Test_record/ACL_ViT16_Exp_ACL_v1/dumps/epochbest
+        Mode: best
+        Number: 10
+        <class 'list'> 5152 [[0.03141244500875473], [0.2843964993953705], [0.42967623472213745], [0.1206812635064125], [0.6309248805046082]]
+        <class 'list'> 5152 ['-0BIyqJj9ZU_000030', '-0UuUoXQUoI_000107', '-2-wdcN5vOw_000017', '-23CeprtibU_000030', '-2Dm0VjW8oM_000001']
+        <class 'list'> 5152 [[0.3117426633834839], [0.07250478118658066], [0.3798030912876129], [0.0345982126891613], [0.28539541363716125]]
 
+        given this sample output, load these three lists into a pandas dataframe (with the same order) to be able to retrieve the sample names with highest/lowest cious/pias
+    3. this is obviously not what i wanted. it is outside the function where i am loading the files, the get_files does not return three lists, and all the processing  of the lists needs to happen within the get_files function keeping the main function clean
 Example usage:
     python retrieve_samples.py --mode best -n 10 ../train_outputs/2214807/Test_record/ACL_ViT16_Exp_ACL_v1/dumps/epochbest
 """
@@ -15,6 +25,7 @@ from pathlib import Path
 import pandas as pd
 
 def get_files(dumps_path, mode, n):
+    """Load and process samples data into a DataFrame."""
     with open(os.path.join(dumps_path, 'cIoUs_ordered_univ_m_i.txt')) as f:
         cious = json.load(f)
 
@@ -24,9 +35,20 @@ def get_files(dumps_path, mode, n):
     with open(os.path.join(dumps_path, 'pIAs_ordered_univ_m_i.txt')) as f:
         pias = json.load(f)
 
-    print(type(cious), len(cious), cious[:5])
-    print(type(samples), len(samples), samples[:5])
-    print(type(pias), len(pias), pias[:5])
+    # Create DataFrame
+    df = pd.DataFrame({
+        'sample_id': samples,
+        'cious': [x[0] if isinstance(x, list) else x for x in cious],
+        'pias': [x[0] if isinstance(x, list) else x for x in pias]
+    })
+
+    # Get best/worst samples based on mode
+    if mode == 'best':
+        result = df.nlargest(n, 'cious')
+    else:  # worst
+        result = df.nsmallest(n, 'cious')
+
+    return result
 
 def parse_arguments():
     """Parse command-line arguments."""
